9b8f233f4f50907c18bd3e2a48a96c654d2e1dd4
Timo Bejan <timo.bejan.ext@iqser.com>
2017-10-28 01:39:26 +0300

    improved snippet security sample

--- snippets/jpa/src/main/java/com/devplant/snippets/jpa/model/Student.java
+++ snippets/jpa/src/main/java/com/devplant/snippets/jpa/model/Student.java
@@ -6,12 +6,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.PostPersist;
-import javax.persistence.PostRemove;
-import javax.persistence.PostUpdate;
-import javax.persistence.PrePersist;
-import javax.persistence.PreRemove;
-import javax.persistence.PreUpdate;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,37 +32,37 @@ public class Student {
     @ManyToOne(fetch = FetchType.EAGER)
     private Trainer trainer;
 
-    @PreRemove
-    protected void preRemove() {
-        log.info("This is called before removing an entity: " + toString());
-    }
-
-    @PostRemove
-    protected void postRemove() {
-        log.info("This is called after removing an entity: " + toString());
-    }
-
-
-    @PreUpdate
-    protected void preUpdate() {
-        log.info("This is called before updating an entity: " + toString());
-    }
-
-    @PostUpdate
-    protected void postUpdate() {
-        log.info("This is called after updating an entity: " + toString());
-    }
-
-    @PrePersist
-    protected void prePersist() {
-        log.info("This is called before persisting an entity: " + toString());
-    }
-
-
-    @PostPersist
-    protected void postPersist() {
-        log.info("This is called after persisting an entity: " + toString());
-    }
-
+//    @PreRemove
+//    protected void preRemove() {
+//        log.info("This is called before removing an entity: " + toString());
+//    }
+//
+//    @PostRemove
+//    protected void postRemove() {
+//        log.info("This is called after removing an entity: " + toString());
+//    }
+//
+//
+//    @PreUpdate
+//    protected void preUpdate() {
+//        log.info("This is called before updating an entity: " + toString());
+//    }
+//
+//    @PostUpdate
+//    protected void postUpdate() {
+//        log.info("This is called after updating an entity: " + toString());
+//    }
+//
+//    @PrePersist
+//    protected void prePersist() {
+//        log.info("This is called before persisting an entity: " + toString());
+//    }
+//
+//
+//    @PostPersist
+//    protected void postPersist() {
+//        log.info("This is called after persisting an entity: " + toString());
+//    }
+//
 
 }
